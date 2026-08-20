2322df3673efd25be2e503e68772dd2af4a720c0
jimj90201-a11y <jimj90201@gmail.com>
2026-08-20 17:57:44 +0530

    Update ElyByLoginFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ElyByLoginFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ElyByLoginFragment.java
@@ -4,13 +4,16 @@ import net.kdt.pojavlaunch.extra.ExtraConstants;
 
 public class ElyByLoginFragment extends OAuthFragment {
     public static final String TAG = "ELYBY_LOGIN_FRAGMENT";
+
     public ElyByLoginFragment() {
-        super("internalredirect",
+        super(
+                "internalredirect",
                 "https://account.ely.by/oauth2/v1" +
-                        "?client_id=mojolauncher2" +
+                        "?client_id=nether-launcher" +
                         "&redirect_uri=internalredirect%3A%2F%2Fcomplete" +
                         "&response_type=code" +
                         "&scope=account_info%20offline_access%20minecraft_server_session",
-                ExtraConstants.ELYBY_LOGIN_TODO);
+                ExtraConstants.ELYBY_LOGIN_TODO
+        );
     }
 }
