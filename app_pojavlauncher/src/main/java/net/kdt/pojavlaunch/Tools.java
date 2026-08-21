c7aeebba6adb0485d8a800324c20e0405edd2f3e
jimj90201-a11y <jimj90201@gmail.com>
2026-08-21 19:36:20 +0530

    Update Tools.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -93,7 +93,7 @@ public final class Tools {
 
     public static final Gson GLOBAL_GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public static final String URL_HOME = "https://google.com";
+    public static final String URL_HOME = "http://mica-team.github.io/nether-guide/";
     public static String NATIVE_LIB_DIR;
     public static String DIR_DATA; //Initialized later to get context
     public static File DIR_CACHE;
